a697ea159ddebea8fbd03c9ada60b38a1f7d767a
Barni <166104424+BabuskaBarn@users.noreply.github.com>
2025-06-03 20:06:51 +0200

    Lighting und ModelView Filter wurden angepasst

--- 3dModelling/src/main/java/at/fhv/sysarch/lab3/pipeline/filters/LightingFilter.java
+++ 3dModelling/src/main/java/at/fhv/sysarch/lab3/pipeline/filters/LightingFilter.java
@@ -1,35 +1,79 @@
 package at.fhv.sysarch.lab3.pipeline.filters;
 
 import at.fhv.sysarch.lab3.obj.Face;
+import at.fhv.sysarch.lab3.pipeline.pipe.Pipe;
 import com.hackoeur.jglm.Vec3;
 
-public class LightingFilter implements PushFilter{
-    private final Vec3 lightPos;
-    private PushFilter next;
+public class LightingFilter implements PushFilter<Face, Face>, PullFilter<Face, Face>{
 
-    public LightingFilter(PushFilter next, Vec3 lightPos){
-        this.next=next;
+    //Position des Lichts
+    private final Vec3 lightPos;
+    private Pipe<Face> next;
+
+    private Pipe<Face> prev;
+
+    public LightingFilter( Vec3 lightPos){
+
         this.lightPos=lightPos;
     }
 
 
     @Override
+    public void setNext(Pipe<Face> next) {
+        this.next = next;
+    }
+
+    @Override
     public void push(Face face) {
-        // Durchschnittsnormale berechnen
-        Vec3 n1 = new Vec3(face.getN1().getX(), face.getN1().getY(), face.getN1().getZ());
-        Vec3 n2 = new Vec3(face.getN2().getX(), face.getN2().getY(), face.getN2().getZ());
-        Vec3 n3 = new Vec3(face.getN3().getX(), face.getN3().getY(), face.getN3().getZ());
 
-        Vec3 normal = n1.add(n2).add(n3).scale(1.0f / 3f).getUnitVector();
+        Face litFace = applyLighting(face);
+        if (next != null) {
+            next.push(litFace);
+        }
 
-        Vec3 v1 = new Vec3(face.getV1().getX(), face.getV1().getY(), face.getV1().getZ());
+
+    }
+
+    private Face applyLighting(Face face) {
+        Vec3 n1 = toVec3(face.getN1());
+        Vec3 n2 = toVec3(face.getN2());
+        Vec3 n3 = toVec3(face.getN3());
+
+        Vec3 normal = n1.add(n2).add(n3).scale(1f / 3f).getUnitVector();
+        Vec3 v1 = toVec3(face.getV1());
         Vec3 lightDir = lightPos.subtract(v1).getUnitVector();
 
         float brightness = Math.max(0f, normal.dot(lightDir));
 
+        // TODO: Hier könntest du brightness speichern (z.B. in einer Wrapperklasse),
+        // aktuell gibt es keine Modifikation am Face-Objekt
 
+        return face;
+    }
 
-        next.push(face);
+    private Vec3 toVec3(com.hackoeur.jglm.Vec4 vec4) {
+        return new Vec3(vec4.getX(), vec4.getY(), vec4.getZ());
+    }
 
+    @Override
+    public void setPrevious(Pipe<Face> prev) {
+        this.prev = prev;
+    }
+
+    @Override
+    public Face pull() {
+        if (prev == null) {
+            return null;
+        }
+        Face face = prev.pull();
+        if (face == null) {
+            return null;
+        }
+        return applyLighting(face);
     }
 }
+
+/* Wir werden die Farbe bei den factorys applyen
+
+
+
